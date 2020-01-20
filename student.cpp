a209186f16afc39457fd6ef98ef61720fbbb018b
rose hall <behindthewallofnoise@gmail.com>
2020-01-20 13:42:14 -0800

    added student.cpp to create full functionality for class

---
+++ student.cpp
@@ -0,0 +1,26 @@
+#include "student.h" // allows access to student header file
+
+using namespace std;
+
+Student::Student(char* newFirstName, char* newLastName, int newID, float newGPA) { // constructor
+  firstName = newFirstName;
+  lastName = newLastName;
+  ID = newID;
+  GPA = newGPA;
+}
+
+char* Student::getFirstName() { // return student's first name
+  return firstName;
+}
+
+char* Student::getLastName() { // return student's last name
+  return lastName;
+}
+
+int Student::getID() { // return student's ID number
+  return ID;
+}
+
+float Student::getGPA() { // return student's ID number
+  return GPA;
+}
